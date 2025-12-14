54e5c13c01214e4c307e3c8a8b6052f851844426
rohans9 <rohansahu345@gmail.com>
2025-12-14 11:20:14 +0530

    commit before dvc

---
+++ pycode.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+df = pd.read_csv('C:\\Users\\Rohan Sahu\\MLOps\\people-1000.csv')
+# print(df.head())
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+file_path = os.path.join(data_dir, 'people-1000.csv')
+df.to_csv(file_path, index=False)
+print(f"File saved to {file_path}")
