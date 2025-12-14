d151210f54d0ca1e87a55b02ec932634ad28669a
rohans9 <rohansahu345@gmail.com>
2025-12-14 11:51:05 +0530

    data -v2 saved

--- pycode.py
+++ pycode.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
 
-df = pd.read_csv('C:\\Users\\Rohan Sahu\\MLOps\\people-1000.csv')
+# df = pd.read_csv('C:\\Users\\Rohan Sahu\\MLOps\\people-1000.csv')
 # print(df.head())
+df = pd.read_csv("C:\\Users\\Rohan Sahu\\MLOps\\data\\people-1000.csv")
 
+df = df.head(100)
 data_dir = 'data'
-os.makedirs(data_dir, exist_ok=True)
+# os.makedirs(data_dir, exist_ok=True)
 
 file_path = os.path.join(data_dir, 'people-1000.csv')
 df.to_csv(file_path, index=False)
